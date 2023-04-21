b14d20b467330cf60b45be5c5a195e8430622238
Yassine Mhirsi <y.mhirsi21318@pi.tn>
2023-04-21 03:40:48 +0100

    bdd
    finished the sign up form

---
+++ add-user.php
@@ -0,0 +1,20 @@
+<?php
+
+include ('connect.php') ;
+
+
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $role=$_POST['role'];
+
+    $req = $pdo->prepare("INSERT INTO user (email, name, password,role) VALUES (:email, :name, :password,:role)");
+
+    $req->bindValue(':email', $email, PDO::PARAM_STR);
+    $req->bindValue(':name', $name, PDO::PARAM_STR);
+    $req->bindValue(':password', $password, PDO::PARAM_STR);
+    $req->bindValue(':role', $role, PDO::PARAM_STR);
+    
+    $req->execute();
+
+?>
